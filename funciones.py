bf5a00c4cd51a9819b883d1d3cd0ab47bd831205
sotelin <valentin.sotelo@manquemavida.cl>
2023-06-14 09:52:54 -0400

    cambié el codigo

--- funciones.py
+++ funciones.py
@@ -1,14 +1,6 @@
-def es_primo(numero):
-    if numero < 2:
-        return False
-    for i in range(2, numero):
-        if numero % i == 0:
-            return False
-    return True
-
-numero = int(input("Ingrese un número: "))
-
-if es_primo(numero):
-    print(f"{numero} es un número primo.")
-else:
-    print(f"{numero} no es un número primo.")
+def es_primo(num):
+    for n in range(2, num):
+        if num % n == 0:
+           return False
+        else:
+            return True
