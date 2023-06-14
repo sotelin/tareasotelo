d8a49b61bf4529ae43e1661918096bc9500ee0e2
sotelin <valentin.sotelo@manquemavida.cl>
2023-06-14 09:55:18 -0400

    agregué el código que me genera la lista de los numeros primos menores al ningresado

--- funciones.py
+++ funciones.py
@@ -1,6 +1,17 @@
-def es_primo(num):
-    for n in range(2, num):
-        if num % n == 0:
-           return False
-        else:
-            return True
+def es_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+    return True
+
+numero = int(input("Ingrese un número: "))
+
+primos = []
+for i in range(2, numero):
+    if es_primo(i):
+        primos.append(i)
+
+print("Números primos menores a", numero, ":")
+print(primos)
