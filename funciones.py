b7b977e74a0d8a5dc403626912bf29512fc0195f
sotelin <valentin.sotelo@manquemavida.cl>
2023-06-14 09:50:27 -0400

    agregue la funcion que me determina si un numero es primo o no

--- funciones.py
+++ funciones.py
@@ -1,2 +1,14 @@
-print("hola")
-print("mholasadl")
+def es_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+    return True
+
+numero = int(input("Ingrese un número: "))
+
+if es_primo(numero):
+    print(f"{numero} es un número primo.")
+else:
+    print(f"{numero} no es un número primo.")
